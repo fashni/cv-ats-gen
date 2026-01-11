b1693b4ac5f18392df1f4aac22e69ce97fa9f5b4
fashni <fashni@tuta.io>
2026-01-11 15:27:02 +0700

    add cli argument to specify port

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,41 @@ mod render;
 mod routes;
 mod sections;
 
+use std::env;
 use tiny_http::Server;
 use routes::handle_request;
 
 fn main() {
-  let server = Server::http("0.0.0.0:8080").unwrap();
-  println!("Server is running on http://localhost:8080");
+  let args: Vec<String> = env::args().collect();
+  let mut port: u16 = 8080;
+
+  let mut i = 1;
+  while i < args.len() {
+    match args[i].as_str() {
+      "-p" | "--port" => {
+        if i+1 < args.len() {
+          match args[i+1].parse::<u16>() {
+            Ok(num) => port = num,
+            Err(_) => {
+              eprintln!("Error: Invalid port number");
+              return;
+            }
+          }
+          i += 1;
+        } else {
+          eprint!("Unspecified port number");
+          return;
+        }
+      },
+      _ => {
+        eprintln!("Unknown arguments: {}", args[i]);
+      }
+    }
+    i += 1;
+  }
+
+  let server = Server::http(format!("0.0.0.0:{}", port)).unwrap();
+  println!("Server is running on http://localhost:{}", port);
 
   for request in server.incoming_requests() {
     let response = handle_request(&request);
